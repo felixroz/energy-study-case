177c27e9def827c4b43df3a758c681809dd98c6e
Marlon Rozindo <marlon.saura@gmail.com>
2022-06-06 00:45:24 -0300

    fixing kubernetes connection

--- repository/helm-charts/airflow/dags/fuel-dag.py
+++ repository/helm-charts/airflow/dags/fuel-dag.py
@@ -25,84 +25,85 @@ with DAG(
     schedule_interval='@daily',
     tags=['development', 's3', 'sensor', 'minio', 'spark', 'operator', 'k8s']
 ) as dag:
+
     staging_diesel_spark_operator = SparkKubernetesOperator(
         task_id='staging_diesel_spark_operator',
         namespace='processing',
         application_file='load_to_staging_diesel.yaml',
-        kubernetes_conn_id='minikube',
+        kubernetes_conn_id='kubeconnect',
         do_xcom_push=True)
 
     monitor_spark_staging_diesel = SparkKubernetesSensor(
         task_id='monitor_spark_staging_diesel',
         namespace="processing",
         application_name="{{ task_instance.xcom_pull(task_ids='staging_diesel_spark_operator')['metadata']['name'] }}",
-        kubernetes_conn_id="minikube")
+        kubernetes_conn_id="kubeconnect")
 
 
     staging_oil_spark_operator = SparkKubernetesOperator(
         task_id='staging_oil_spark_operator',
         namespace='processing',
         application_file='load_to_staging_oil.yaml',
-        kubernetes_conn_id='minikube',
+        kubernetes_conn_id='kubeconnect',
         do_xcom_push=True)
 
     monitor_spark_staging_oil = SparkKubernetesSensor(
         task_id='monitor_spark_staging_oil',
         namespace="processing",
         application_name="{{ task_instance.xcom_pull(task_ids='staging_oil_spark_operator')['metadata']['name'] }}",
-        kubernetes_conn_id="minikube")
+        kubernetes_conn_id="kubeconnect")
 
     bronze_diesel_spark_operator = SparkKubernetesOperator(
         task_id='bronze_diesel_spark_operator',
         namespace='processing',
         application_file='load_to_bronze_diesel.yaml',
-        kubernetes_conn_id='minikube',
+        kubernetes_conn_id='kubeconnect',
         do_xcom_push=True)
     
     monitor_spark_bronze_diesel = SparkKubernetesSensor(
         task_id='monitor_spark_bronze_diesel',
         namespace="processing",
         application_name="{{ task_instance.xcom_pull(task_ids='bronze_diesel_spark_operator')['metadata']['name'] }}",
-        kubernetes_conn_id="minikube")
+        kubernetes_conn_id="kubeconnect")
 
     bronze_oil_spark_operator = SparkKubernetesOperator(
         task_id='bronze_oil_spark_operator',
         namespace='processing',
         application_file='load_to_bronze_oil.yaml',
-        kubernetes_conn_id='minikube',
+        kubernetes_conn_id='kubeconnect',
         do_xcom_push=True)
     
     monitor_spark_bronze_oil = SparkKubernetesSensor(
         task_id='monitor_spark_bronze_oil',
         namespace="processing",
         application_name="{{ task_instance.xcom_pull(task_ids='bronze_oil_spark_operator')['metadata']['name'] }}",
-        kubernetes_conn_id="minikube")
+        kubernetes_conn_id="kubeconnect")
 
     silver_fuel_spark_operator = SparkKubernetesOperator(
         task_id='silver_fuel_spark_operator',
         namespace='processing',
         application_file='load_to_silver.yaml',
-        kubernetes_conn_id='minikube',
+        kubernetes_conn_id='kubeconnect',
         do_xcom_push=True)
     
     monitor_spark_silver_fuel = SparkKubernetesSensor(
         task_id='monitor_spark_silver_fuel',
         namespace="processing",
         application_name="{{ task_instance.xcom_pull(task_ids='silver_fuel_spark_operator')['metadata']['name'] }}",
-        kubernetes_conn_id="minikube")
+        kubernetes_conn_id="kubeconnect")
 
     gold_fuel_spark_operator = SparkKubernetesOperator(
         task_id='gold_fuel_spark_operator',
         namespace='processing',
         application_file='load_to_gold.yaml',
-        kubernetes_conn_id='minikube',
+        kubernetes_conn_id='kubeconnect',
         do_xcom_push=True)
     
     monitor_spark_gold_fuel = SparkKubernetesSensor(
         task_id='monitor_spark_gold_fuel',
         namespace="processing",
         application_name="{{ task_instance.xcom_pull(task_ids='gold_fuel_spark_operator')['metadata']['name'] }}",
-        kubernetes_conn_id="minikube")
+        kubernetes_conn_id="kubeconnect")
 
     chain([staging_diesel_spark_operator, staging_oil_spark_operator ]
         , [monitor_spark_staging_diesel, monitor_spark_staging_oil]
